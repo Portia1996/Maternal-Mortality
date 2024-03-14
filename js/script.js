7f697038e1b75776f900f0ad98be5f8296745641
Porsha Serwaa <porshaserwaa@Porshas-MacBook-Pro.local>
2024-03-14 03:32:48 +0000

    pushing content

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const darkMode = document.getElementById('darkMode');
-const lightMode = document.getElementById('lightMode');
+const navbar = document.querySelector('.navbar')
 
-
-
-darkMode.addEventListener('click', ()=>{
-
-    document.querySelector('body').style.backgroundColor = 'black';
-})
-
-
-lightMode.addEventListener('click', ()=>{
-    document.querySelector('body').style.backgroundColor = 'white';
-})
+navbar.innerHTML = `
+<div class="logo">
+          <span class="logo-text">PORTIA YEBOAH</span>
+        </div>
+        <!-- <div class="img"><img src="" alt="" /></div> -->
+        <div class="menu">
+          <a href="/" class="">Home</a>
+          <a href="/pages/video.html" class="">Videos</a>
+          <a href="./pages/blog.html" class="">Blog</a>
+          <a href="#" class="">Portfolio</a>
+          <a href="" class="">Contact</a>
+        </div>`
